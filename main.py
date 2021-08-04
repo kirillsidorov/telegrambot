8c7e3695e839f32102bc48d715bd362d9b33a277
Kirill <kirill@MacBook-Pro.local>
2021-08-04 21:45:59 +0300

    Procfile and requirements

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ import feedparser
 import time
 import config
 import ssl
-import requests
 from telebot import types
 from datetime import datetime
 from time import mktime
-
 
 ssl._create_default_https_context = ssl._create_unverified_context
 bot = telebot.TeleBot(config.TOKEN)
 
 def main():
     posts = getPosts()
-    currentDay = datetime.today().day
+    current_day = datetime.today().day
     for post in posts:
         dt = datetime.fromtimestamp(mktime(post.published_parsed))
-        if currentDay == dt.day:
+        if current_day == dt.day:
             sendPost(post)
         time.sleep(15)
 
@@ -31,12 +29,10 @@ def sendPost(post):
     button_link = types.InlineKeyboardMarkup()
     url_button = types.InlineKeyboardButton("Подробнее", link)
     button_link.add(url_button)
-    bot.send_message(config.channel, html_msg, parse_mode='html', reply_markup=button_link)
-
+    bot.send_message(config.CHANNELNAME, html_msg, parse_mode='html', reply_markup=button_link)
 
 def getPosts():
-    news_feed = feedparser.parse(config.htmlurlarticles)
+    news_feed = feedparser.parse(config.HTMLUrlArticles)
     return news_feed.entries
-
 
 main()
